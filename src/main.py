93b8cb2e4c12795ef34f9bf9fc4db9c08792d61a
Pedro B Cavalcante <pedrobenevides3243@gmail.com>
2024-11-13 20:13:36 -0300

    salvo os dados da carteira bnb

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
-from core.database.db_manager import DBManager
+from core.database.bnb_wallet_db_manager import BNBWalletDBManager
 
 
 def main():
-    # Inicializa o gerenciador do banco de dados
-    db_manager = DBManager()
+    # Inicializa o gerenciador do banco de dados para a carteira BNB
+    bnb_wallet_db = BNBWalletDBManager()
 
-    # Calcula e exibe todos os ativos com o percentual de cada um em relação ao total da carteira
-    assets_with_percentages = db_manager.get_asset_percentages()
-    for asset in assets_with_percentages:
-        print(
-            f"Ativo: {asset['asset_name']}, Pontos: {asset['points']}, "
-            f"Preço Médio: {asset['average_price']}, Percentual da Carteira: {asset['percentage']:.2f}%"
-        )
+    # Salva os saldos de tokens
+    bnb_wallet_db.save_token_balance("bnb", 0.00053376)
+    bnb_wallet_db.save_token_balance("btc", 0.11672992)
+    bnb_wallet_db.save_token_balance("eth", 2.99979039)
+
+    # Exibe todos os saldos salvos
+    token_balances = bnb_wallet_db.get_all_token_balances()
+    print("Saldos na carteira BNB:")
+    for token in token_balances:
+        print(f"{token['token_name'].upper()}: Quantidade: {token['quantity']}")
 
 
 if __name__ == "__main__":
